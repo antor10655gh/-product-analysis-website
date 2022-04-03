6193f918ff96bb53d5c505c77f5a24904741f994
antor10655gh <creativefreelancerantor@gmail.com>
2022-04-03 23:29:53 +0600

    add header logo and navlink

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./components/About/About";
+import Blog from "./components/Blog/Blog";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Reviews from "./components/Reviews/Reviews";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Routes>
+        <Route to="/" element={<Home></Home>}></Route>
+        <Route to="/home" element={<Home></Home>}></Route>
+        <Route to="/reviews" element={<Reviews></Reviews>}></Route>
+        <Route to="/dashboard" element={<Dashboard></Dashboard>}></Route>
+        <Route to="/blog" element={<Blog></Blog>}></Route>
+        <Route to="/about" element={<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import CustomLink from "../CustomLink/CustomLink";
+import logo from "../images/mobile-logo.png";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="container" style={{ backgroundColor: "black" }}>
+      <div className="row row-cols-1 row-cols-lg-2 d-flex align-items-center">
+        <div className="col header-banner">
+          <img src={logo} alt="" />
+        </div>
+        <div className="col">
+          <nav>
+            <CustomLink to="/home">Home</CustomLink>
+            <CustomLink to="/reviews">Reviews</CustomLink>
+            <CustomLink to="/dashboard">Dashboard</CustomLink>
+            <CustomLink to="/blog">Blog</CustomLink>
+            <CustomLink to="/about">About</CustomLink>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
